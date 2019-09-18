062f4fcc45be938bdd153a0e399da1b74ef881b6
sleald <42923316+sleald@users.noreply.github.com>
2019-09-19 00:21:28 +1000

    Add files via upload

---
+++ pandasDataFrameDeeper.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+# pass in column names for each CSV
+u_cols = ['user_id', 'age', 'sex', 'occupation', 'zip_code']
+users = pd.read_csv('ml-100k/u.user', sep='|', names=u_cols,
+                    encoding='latin-1')
+
+r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
+ratings = pd.read_csv('ml-100k/u.data', sep='\t', names=r_cols,
+                      encoding='latin-1')
+
+# the movies file contains columns indicating the movie's genres
+# let's only load the first five columns of the file with usecols
+m_cols = ['movie_id', 'title', 'release_date', 'video_release_date', 'imdb_url']
+movies = pd.read_csv('ml-100k/u.item', sep='|', names=m_cols, usecols=range(5),
+                     encoding='latin-1')
+
+#Inspection
+movies.info()
+
+movies.memory_usage
+movies.dtypes
